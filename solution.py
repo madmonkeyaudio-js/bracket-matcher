659081fb23e62f0631c6f2182ac7c04df99874cf
madmonkeyaudio-js <jonathangross@Jonathans-MacBook-Pro.local>
2019-09-30 08:43:18 -0700

    hw

--- solution.py
+++ solution.py
@@ -1,2 +1,44 @@
-def bracket_matcher(input):
-  pass
+def bracket_matcher(my_input):
+  start = ["[","{","("] 
+  close = ["]","}",")"] 
+  stack = [] 
+  for i in my_input: 
+      if i in start: 
+          stack.append(i) 
+      elif i in close: 
+          position = close.index(i) 
+          if ((len(stack) > 0) and (start[position] == stack[len(stack)-1])): 
+              stack.pop() 
+          else: 
+              return False
+  if len(stack) == 0: 
+      return True
+
+
+print(bracket_matcher('abc(123)'))
+# returns true
+
+print(bracket_matcher('a[b]c(123'))
+# returns false -- missing closing parens
+
+print(bracket_matcher('a[bc(123)]'))
+# returns true
+
+print(bracket_matcher('a[bc(12]3)'))
+# returns false -- improperly nested
+
+print(bracket_matcher('a{b}{c(1[2]3)}'))
+# returns true
+
+print(bracket_matcher('a{b}{c(1}[2]3)'))
+# returns false -- improperly nested
+
+print(bracket_matcher('()'))
+# returns true
+
+print(bracket_matcher('[]]'))
+# returns false - no opening bracket to correspond with last character
+
+print(bracket_matcher('abc123yay'))
+# returns true -- no brackets = correctly matched
+
